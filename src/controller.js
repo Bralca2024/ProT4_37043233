70db569688bf6ff135319264f0f78eafc487013a
Bralca <fernandocabraldev@gmail.com>
2024-08-14 16:30:30 -0300

    Se agregaron los comentarios correspondientes a lo que realiza cada una de las partes del código

--- src/controller.js
+++ src/controller.js
@@ -4,18 +4,19 @@ class BooksController {
 
   async getBookById(req, res) {
     try {
+      // Se obtiene el ID del libro desde los parámetros de la URL
       const bookId = req.params.id;
-
+      // Verifica se si proporcionó un ID
       if (!bookId) {
         return res.status(400).json({ message: 'ID del libro es requerido' });
       }
-
+      // Consulta el libro en la base de datos
       const [result] = await pool.query('SELECT * FROM libros WHERE id = ?', [bookId]);
-
+      // Verifica si se encontró el libro
       if (result.length === 0) {
         return res.status(404).json({ message: 'Libro no encontrado' });
       }
-
+      // Devuelve el libro encontrado
       res.status(200).json(result[0]);
     } catch (error) {
       res.status(500).json({ message: `${error.message}` });
@@ -34,17 +35,17 @@ class BooksController {
   async addBook(req, res) {
     try {
       const book = req.body;
-
+      // Validación de campos
       if (!book.titulo || !book.autor || !book.categoria || !book.año_publicacion || !book.isbn) {
         return res.status(400).json({ message: 'Todos los campos son obligatorios.' });
       }
-
+      // Se utiliza un regExp para validar el formato de año_publicacion (YYYY-MM-DD)
       const yearPublicationRegex = /^\d{4}-\d{2}-\d{2}$/;
 
       if (!yearPublicationRegex.test(book.año_publicacion)) {
         return res.status(400).json({ message: 'El formato de año_publicacion debe ser YYYY-MM-DD.' });
       }
-
+      // Se verifica si el ISBN ya existe en la base de datos
       const [existingBook] = await pool.query(
         'SELECT * FROM libros WHERE isbn = ?',
         [book.isbn]
@@ -53,7 +54,7 @@ class BooksController {
       if (existingBook.length > 0) {
         return res.status(400).json({ message: 'El ISBN ya existe en la base de datos.' });
       }
-
+      // Realiza un inserción de los elementos en la base de datos
       const [result] = await pool.query(
         `INSERT INTO libros (titulo, autor, categoria, año_publicacion, isbn) VALUES (?, ?, ?, ?, ?)`,
         [book.titulo, book.autor, book.categoria, book.año_publicacion, book.isbn]
@@ -68,17 +69,17 @@ class BooksController {
   async deleteByISBN(req, res) {
     try {
       const { isbn } = req.body;
-
+      // Verifica que se haya proporcionado un ISBN en el cuerpo de la solicitud
       if (!isbn) {
         return res.status(400).json({ error: "ISBN del libro es requerido" });
       }
-
+      // Realiza la consulta para eliminar un libro
       const [result] = await pool.query(`DELETE FROM Libros WHERE isbn = ?`, [isbn]);
-
+      // Verifica si se eliminó correctamente el registro de la base de datos
       if (result.affectedRows === 0) {
         return res.status(404).json({ error: "Libro no encontrado" });
       }
-
+      // Muestra el número de registros eliminados
       res.json({ mensaje: "Registro eliminado", registrosEliminados: result.affectedRows });
     } catch (error) {
       console.error("Error al eliminar libro:", error);
@@ -88,19 +89,20 @@ class BooksController {
 
   async updateBook(req, res) {
     try {
+      // Se obtiene el ID del libro por los parámetros de la solicitud
       const bookId = req.params.id;
 
       const book = req.body;
-
+      // Validación de campos
       if (!book.titulo || !book.autor || !book.categoria || !book.año_publicacion || !book.isbn) {
         return res.status(400).json({ message: 'Todos los campos son obligatorios.' });
       }
-
+      // Se utiliza un regExp para validar el formato de año_publicacion (YYYY-MM-DD)
       const yearPublicationRegex = /^\d{4}-\d{2}-\d{2}$/;
       if (!yearPublicationRegex.test(book.año_publicacion)) {
         return res.status(400).json({ message: 'El formato de año_publicacion debe ser YYYY-MM-DD.' });
       }
-
+      // Verifica si sl libro existe en la base de datos
       const [existingBookCheck] = await pool.query(
         'SELECT * FROM libros WHERE id = ?',
         [bookId]
@@ -109,7 +111,7 @@ class BooksController {
       if (existingBookCheck.length === 0) {
         return res.status(404).json({ message: 'El libro con el ID que ingresó no existe.' });
       }
-
+      // Verificar si el ISBN ya existe en la base de datos (excluyendo el libro actual)
       const [duplicateISBNCheck] = await pool.query(
         'SELECT * FROM libros WHERE isbn = ? AND id <> ?',
         [book.isbn, bookId]
@@ -118,7 +120,7 @@ class BooksController {
       if (duplicateISBNCheck.length > 0) {
         return res.status(400).json({ message: 'El ISBN ya existe en la base de datos.' });
       }
-
+      // Actualiza el libro en la base de datos
       await pool.query(
         `UPDATE libros SET titulo = ?, autor = ?, categoria = ?, año_publicacion = ?, isbn = ? WHERE id = ?`,
         [book.titulo, book.autor, book.categoria, book.año_publicacion, book.isbn, bookId]
